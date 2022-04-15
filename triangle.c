a571f4c31eb76d8c108e0c3b269cc42368ee9731
Harutsuki0627 <90494875+Harutsuki0627@users.noreply.github.com>
2022-04-15 15:14:18 +0800

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -4,8 +4,13 @@ main () {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n-i; j++) {
-            printf("*");
+            if(i % 2 == 0){
+               printf("*");
+            }else{
+               printf("+");
+            }
         }
         printf("\n");
     }
 }
+
